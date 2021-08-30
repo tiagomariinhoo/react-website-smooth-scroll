241ec01b9a94595aca8817c485231221749e6141
Tiago Marinho <tiago.marinho.797@gmail.com>
2021-08-30 18:04:06 -0300

    Started info section

--- src/components/Navbar/NavbarElements.js
+++ src/components/Navbar/NavbarElements.js
@@ -3,20 +3,19 @@ import { Link as LinkR } from 'react-router-dom'
 import { Link as LinkS } from 'react-scroll'
 
 export const Nav = styled.nav`
-  background: #000;
+  background: ${({scrollNav}) => (scrollNav ? '#000' : 'transparent')};
   height: 80px;
-  /* margin-top: -80px; */
-  display: flex;
+  margin-top: -80px;
+  display:flex;
   justify-content: center;
   align-items: center;
   font-size: 1rem;
   position: sticky;
-  top: 0;
-  z-index: 10;
-
+  top:0;
+  z-index:10;
   @media screen and (max-width: 960px) {
     transition: 0.8s all ease;
-  }  
+  }
 `
 
 export const NavbarContainer = styled.div`
@@ -28,8 +27,8 @@ export const NavbarContainer = styled.div`
   padding: 0 24px;
   max-width: 1100px;
 `
-export const NavLogo = styled(LinkR)`
-  color: #fff;
+export const NavLogo = styled(LinkR) `
+  color:#fff;
   justify-self: flex-start;
   cursor: pointer;
   font-size: 1.5rem;
@@ -53,24 +52,19 @@ export const MobileIcon = styled.div`
     color: #fff;
   }
 `
-
 export const NavMenu = styled.ul`
-  display: flex;
-  align-items: center;
+  display:flex;
+  justify-content: center;
   list-style: none;
   text-align: center;
   margin-right: -22px;
-
   @media screen and (max-width: 768px) {
     display: none;
   }
 `
-
 export const NavItem = styled.li`
   height: 80px;
 `
-
-// React Scrolling
 export const NavLinks = styled(LinkS)`
   color: #fff;
   display: flex;
@@ -79,21 +73,17 @@ export const NavLinks = styled(LinkS)`
   padding: 0 1rem;
   height: 100%;
   cursor: pointer;
-
   &.active {
-    border-bottom: 3px solid #01bf71;
+    border-bottom: 3px solid #01bf71
   }
 `
-
 export const NavBtn = styled.nav`
   display: flex;
   align-items: center;
-
   @media screen and (max-width: 768px) {
     display: none;
   }
 `
-
 export const NavBtnLink = styled(LinkR)`
   border-radius: 50px;
   background: #01bf71;
@@ -106,7 +96,6 @@ export const NavBtnLink = styled(LinkR)`
   cursor: pointer;
   transition: all 0.2s ease-in-out;
   text-decoration: none;
-
   &:hover {
     transition: all 0.2s ease-in-out;
     background: #fff;
